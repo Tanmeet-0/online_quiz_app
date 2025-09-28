9ebdf068dfa6a2752bac049b08eeda4605ea01d6
Tanmeet Singh Chhabra <tanmeet.chhabra.0@gmail.com>
2025-09-28 09:10:55 +0530

    defined models for quizzes

--- django_backend/quiz/models.py
+++ django_backend/quiz/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Quiz(models.Model):
+    pass
+
+class Question(models.Model):
+    part_of_quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="questions")
+    description = models.CharField()
+    correct_option = models.OneToOneField("Option", on_delete=models.CASCADE, null=True, blank=True)
+
+class Option(models.Model):
+    part_of_question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="options")
+    value = models.CharField()

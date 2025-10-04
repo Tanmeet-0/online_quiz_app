c0ca028da2e4d8cfd4bd66f59c39ee66e7999b4a
Tanmeet Singh Chhabra <tanmeet.chhabra.0@gmail.com>
2025-10-05 01:03:37 +0530

    added an icon for the website

--- react_frontend/app/entry.client.tsx
+++ react_frontend/app/entry.client.tsx
@@ -3,11 +3,12 @@ import { hydrateRoot } from "react-dom/client";
 import { HydratedRouter } from "react-router/dom";
 
 startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <title>Quizzes</title>
-      <HydratedRouter />
-    </StrictMode>,
-  );
+    hydrateRoot(
+        document,
+        <StrictMode>
+            <title>Quizzes</title>
+            <link rel="icon" type="image/png" href="./favicon.png"></link>
+            <HydratedRouter />
+        </StrictMode>,
+    );
 });
